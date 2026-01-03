2753028205395e79a6cfa4a81dba463ae38cf2af
Deepak Kumar <deepak.kumar97@cipl.org.in>
2026-01-03 11:40:25 +0530

    my users rest api initial complaint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
